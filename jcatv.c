6e9242001b56b0943199bce7950986ca77e5dd11
James H <nus4xd@gmail.com>
2023-05-19 21:36:31 +0700

    newest

--- jcatv.c
+++ jcatv.c
@@ -11,178 +11,35 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#ifdef _GNU_SOURCE
-#	define HAS_MEMMEM
-#	define HAS_STPCPY
-#	define HAS_STRCASESTR
-#elif defined(__GLIBC__)
-#	if __GLIBC__ >= 2 && __GLIBC_MINOR__ >= 1 && _POSIX_C_SOURCE >= 200809
-#		define HAS_STPCPY
-#	endif
-#endif /* HAS_MEMMEM, HAS_STPCPY */
-
-#if defined(__GNUC__) || defined(__clang__)
-#	define INLINE __attribute__((always_inline)) inline
-#	if __has_attribute(pure)
-#		define PURE __attribute__((pure))
-#	else
-#		define PURE
-#	endif /* PURE */
-#	if __has_attribute(const)
-#		define CONST __attribute__((const))
-#	else
-#		define CONST
-#	endif /* CONST */
-#	if __has_attribute(flatten)
-#		define FLATTEN __attribute__((flatten))
-#	else
-#		define FLATTEN
-#	endif /* FLATTEN */
-#elif defined(_MSC_VER)
-#	define INLINE __forceinline inline
-#	define PURE __declspec(noalias)
-#	define CONST __declspec(restrict)
-#	define FLATTEN
-#else
-#	define INLINE inline
-#	define PURE
-#	define CONST
-#	define FLATTEN
-#endif /* __GNUC__ || __clang__ || _MSC_VER */
-
-#if (defined(__GNUC__) && (__GNUC__ >= 3)) || (defined(__clang__) && __has_builtin(__builtin_expect))
-#	define likely(x) __builtin_expect(!!(x), 1)
-#	define unlikely(x) __builtin_expect(!!(x), 0)
-#else
-#	define likely(x) (x)
-#	define unlikely(x) (x)
-#endif /* __has_builtin(__builtin_expect) */
-
-#define CASE_PRINTABLE \
-	case 32:       \
-	case 33:       \
-	case 34:       \
-	case 35:       \
-	case 36:       \
-	case 37:       \
-	case 38:       \
-	case 39:       \
-	case 40:       \
-	case 41:       \
-	case 42:       \
-	case 43:       \
-	case 44:       \
-	case 45:       \
-	case 46:       \
-	case 47:       \
-	case 48:       \
-	case 49:       \
-	case 50:       \
-	case 51:       \
-	case 52:       \
-	case 53:       \
-	case 54:       \
-	case 55:       \
-	case 56:       \
-	case 57:       \
-	case 58:       \
-	case 59:       \
-	case 60:       \
-	case 61:       \
-	case 62:       \
-	case 63:       \
-	case 64:       \
-	case 65:       \
-	case 66:       \
-	case 67:       \
-	case 68:       \
-	case 69:       \
-	case 70:       \
-	case 71:       \
-	case 72:       \
-	case 73:       \
-	case 74:       \
-	case 75:       \
-	case 76:       \
-	case 77:       \
-	case 78:       \
-	case 79:       \
-	case 80:       \
-	case 81:       \
-	case 82:       \
-	case 83:       \
-	case 84:       \
-	case 85:       \
-	case 86:       \
-	case 87:       \
-	case 88:       \
-	case 89:       \
-	case 90:       \
-	case 91:       \
-	case 92:       \
-	case 93:       \
-	case 94:       \
-	case 95:       \
-	case 96:       \
-	case 97:       \
-	case 98:       \
-	case 99:       \
-	case 100:      \
-	case 101:      \
-	case 102:      \
-	case 103:      \
-	case 104:      \
-	case 105:      \
-	case 106:      \
-	case 107:      \
-	case 108:      \
-	case 109:      \
-	case 110:      \
-	case 111:      \
-	case 112:      \
-	case 113:      \
-	case 114:      \
-	case 115:      \
-	case 116:      \
-	case 117:      \
-	case 118:      \
-	case 119:      \
-	case 120:      \
-	case 121:      \
-	case 122:      \
-	case 123:      \
-	case 124:      \
-	case 125:      \
-	case 126:
+#include "jcatv.h"
 
 enum {
 	DEBUG = 0,
 	MAX_LINE_LEN = 4096,
 	MAX_PATH_LEN = 4096,
-	MAX_BUF_SZ = 640000000,
 };
 
 char ln[MAX_LINE_LEN];
-struct stat fst;
-char *fbuf;
 int NL;
+size_t fuldirlen;
+char *fuldir;
 
-static INLINE void append(char *fulpath, const char *dname, size_t dlen, const char *filename)
+static INLINE void append(char *path, const char *dir, size_t dlen, const char *filename)
 {
-	memcpy(fulpath, dname, dlen);
-	*(fulpath += dlen) = '/';
-	memcpy(++fulpath, filename, dlen + 1);
+	memcpy(path, dir, dlen);
+	*(path += dlen) = '/';
+	memcpy(++path, filename, dlen + 1);
 }
 
-static INLINE char *appendp(char *fulpath, const char *dname, size_t dlen, const char *filename)
+static INLINE char *appendp(char *path, const char *dir, size_t dlen, const char *filename)
 {
-	memcpy(fulpath, dname, dlen);
-	*(fulpath += dlen) = '/';
+	memcpy(path, dir, dlen);
+	*(path += dlen) = '/';
 #ifdef HAS_STPCPY
-	return stpcpy(++fulpath, filename);
+	return stpcpy(++path, filename);
 #else
 	dlen = strlen(filename);
-	return (char *)memcpy(++fulpath, filename, dlen + 1) + dlen;
+	return (char *)memcpy(++path, filename, dlen + 1) + dlen;
 #endif /* HAS_STPCPY */
 }
 
@@ -191,24 +48,26 @@ static INLINE void catv(const char *filename)
 	FILE *fp = fopen(filename, "r");
 	if (unlikely(!fp))
 		return;
+	NL = 0;
 #if DEBUG == 1
 	printf("filename: %s\n", filename);
 #endif /* DEBUG */
-	int LN = 0;
+#ifdef HAS_FGETS_UNLOCKED
+	while (fgets_unlocked(ln, MAX_LINE_LEN, fp)) {
+#else
 	while (fgets(ln, MAX_LINE_LEN, fp)) {
-		++LN;
+#endif /* _GNU_SOURCE */
+		++NL;
 		for (char *lp = ln;; ++lp) {
 			switch (*lp) {
 				CASE_PRINTABLE
-			case '\t':
-				continue;
-			default:
-				goto OUT;
+			case '\t': continue;
+			default: goto OUT;
 			case '\n':;
 			}
 			break;
 		}
-		printf("%s:%d:%s", filename, LN, ln);
+		printf("%s:%d:%s", filename + fuldirlen + 1, NL, ln);
 	}
 OUT:
 	fclose(fp);
@@ -235,9 +94,9 @@ OUT:
 				action;                \
 		}
 
-static int findall(const char *dname, const size_t dlen)
+static int findall(const char *dir, const size_t dlen)
 {
-	DIR *dp = opendir(dname);
+	DIR *dp = opendir(dir);
 	if (unlikely(!dp))
 		return 0;
 	struct dirent *ep;
@@ -252,23 +111,23 @@ static int findall(const char *dname, const size_t dlen)
 #ifdef _DIRENT_HAVE_D_TYPE
 		switch (ep->d_type) {
 		case DT_REG:
-			append(fulpath, dname, dlen, ep->d_name);
+			append(fulpath, dir, dlen, ep->d_name);
 			catv(fulpath);
 			break;
 		case DT_DIR:
 			/* skip . , .., .git, .vscode */
 			IF_EXCLUDED_DO(ep->d_name, continue)
-			findall(fulpath, appendp(fulpath, dname, dlen, ep->d_name) - fulpath);
+			findall(fulpath, appendp(fulpath, dir, dlen, ep->d_name) - fulpath);
 		}
 #else
-		if (unlikely(stat(dname, &st)))
+		if (unlikely(stat(dir, &st)))
 			return 0;
 		if (S_ISREG(st.st_mode)) {
-			append(fulpath, dname, dlen, ep->d_name);
+			append(fulpath, dir, dlen, ep->d_name);
 			catv(fulpath);
 		} else if (S_ISDIR(st.st_mode)) {
 			IF_EXCLUDED_DO(ep->d_name, continue)
-			findall(fulpath, appendp(fulpath, dname, dlen, ep->d_name) - fulpath);
+			findall(fulpath, appendp(fulpath, dir, dlen, ep->d_name) - fulpath);
 		}
 #endif /* _DIRENT_HAVE_D_TYPE */
 #if DEBUG == 1
@@ -281,77 +140,14 @@ static int findall(const char *dname, const size_t dlen)
 
 int main(int argc, char **argv)
 {
-	size_t dlen;
-	char *dname = argv[1];
-	fbuf = malloc(MAX_BUF_SZ);
-	if (!argv[1]) {
+	if (!(fuldir = argv[1])) {
 		char cwd[MAX_PATH_LEN];
-		dname = getcwd(cwd, MAX_PATH_LEN);
-		dlen = strlen(dname);
-		findall(dname, dlen);
+		fuldir = getcwd(cwd, MAX_PATH_LEN);
+		fuldirlen = strlen(fuldir);
+		findall(fuldir, fuldirlen);
 	} else {
-		dlen = strlen(dname);
-		findall(dname, dlen);
+		fuldirlen = strlen(fuldir);
+		findall(fuldir, fuldirlen);
 	}
 	return 0;
 }
-
-/* segfaults sometimes */
-
-/* static INLINE void print_lines(const char *filename) */
-/* { */
-/* 	FILE *fp = fopen(filename, "r"); */
-/* 	if (unlikely(!fp)) */
-/* 		return; */
-/* 	NL = 0; */
-/* 	while (fgets(fbuf, MAX_LINE_LEN, fp)) { */
-/* 		++NL; */
-/* 		for (char *lp = fbuf;; ++lp) { */
-/* 			switch (*lp) { */
-/* 			CASE_PRINTABLE */
-/* 			case '\t': */
-/* 				continue; */
-/* 			default: */
-/* 				goto OUT; */
-/* 			case '\n':; */
-/* 			} */
-/* 			break; */
-/* 		} */
-/* 		printf("%s:%d:%s", filename, NL, fbuf); */
-/* 	} */
-/* OUT: */
-/* 	fclose(fp); */
-/* } */
-
-/* static INLINE void catv_f(const char *filename) */
-/* { */
-/* 	if (unlikely(stat(filename, &fst))) */
-/* 		return; */
-/* 	const size_t file_size = fst.st_size; */
-/* 	if (unlikely(!file_size)) */
-/* 		return; */
-/* 	if (unlikely(file_size > MAX_BUF_SZ)) */
-/* 		print_lines(filename); */
-/* 	FILE *fp = fopen(filename, "r"); */
-/* 	if (unlikely(!fp)) */
-/* 		return; */
-/* 	fread(fbuf, 1, file_size, fp); */
-/* 	*(fbuf + file_size) = '\0'; */
-/* 	if (strlen(fbuf) == file_size) { */
-/* 		NL = 1; */
-/* 		printf("%s:%d", filename, NL); */
-/* 		for (char *p = fbuf;; ++p) { */
-/* 			switch (*p) { */
-/* 			case '\n': */
-/* 				printf("\n%s:%d:", filename, ++NL); */
-/* 				continue; */
-/* 			default: */
-/* 				putchar(*p); */
-/* 				continue; */
-/* 			case '\0':; */
-/* 			} */
-/* 			break; */
-/* 		} */
-/* 	} */
-/* 	fclose(fp); */
-/* } */
