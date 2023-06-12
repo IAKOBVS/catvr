a7e4f4243b24724b6427bad004cc637c440a184c
James H <nus4xd@gmail.com>
2023-06-12 09:24:37 +0700

    newest

--- src/rgrep.c
+++ src/rgrep.c
@@ -106,7 +106,7 @@ static INLINE void fgrep(const char *ptn, const char *filename, const size_t ptn
 
 	do {
 
-#define LOOP                                              \
+#define LOOP_FGREP                                        \
 		switch (*g_lnp = getc(fp)) {              \
 		CASE_UPPER                                \
 			*g_lnlowerp = *g_lnp - 'A' + 'a'; \
@@ -129,10 +129,10 @@ static INLINE void fgrep(const char *ptn, const char *filename, const size_t ptn
 		}                                         \
 		++g_lnp, ++g_lnlowerp
 
-		LOOP;
-		LOOP;
-		LOOP;
-		LOOP;
+		LOOP_FGREP;
+		LOOP_FGREP;
+		LOOP_FGREP;
+		LOOP_FGREP;
 	} while (MAX_LINE_LEN - 4 > (g_lnp - g_ln));
 OUT:
 	fclose(fp);
@@ -161,13 +161,13 @@ OUT:
 			break;                 \
 		}
 
-#define DO_REG(FUNC_REG, USE_LEN)                                                                                      \
+#define FIND_FGREP_DO_REG(FUNC_REG, USE_LEN)                                                                           \
 	if (USE_LEN)                                                                                                   \
 		FUNC_REG(ptn, fulpath, ptnlen, appendp(fulpath, dir, dlen, ep->d_name) - (fulpath + g_fuldirlen) - 1); \
 	else                                                                                                           \
 		FUNC_REG(ptn, fulpath, 0, 0)
 
-#define DO_DIR(FUNC_SELF)                                                                           \
+#define FIND_FGREP_DO_DIR(FUNC_SELF)                                                                \
 	IF_EXCLUDED_DO(ep->d_name, continue)                                                        \
 	if (pid > 0) {                                                                              \
 		if (g_child_tot == g_child_max) {                                                   \
@@ -187,15 +187,15 @@ OUT:
 
 #ifdef _DIRENT_HAVE_D_TYPE
 
-#define IF_DIR_RECUR_IF_REG_DO(FUNC_SELF, FUNC_REG, USE_LEN) \
-	switch (ep->d_type) {                                \
-		case DT_REG:                                 \
-			DO_REG(FUNC_REG, USE_LEN);           \
-			break;                               \
-		case DT_DIR:                                 \
-			/* skip . , .., .git, .vscode */     \
-			DO_DIR(FUNC_SELF);                   \
-	}                                                    \
+#define IF_DIR_RECUR_IF_REG_DO(FUNC_SELF, FUNC_REG, USE_LEN)  \
+	switch (ep->d_type) {                                 \
+		case DT_REG:                                  \
+			FIND_FGREP_DO_REG(FUNC_REG, USE_LEN); \
+			break;                                \
+		case DT_DIR:                                  \
+			/* skip . , .., .git, .vscode */      \
+			FIND_FGREP_DO_DIR(FUNC_SELF);         \
+	}                                                     \
 
 #else
 
@@ -203,9 +203,9 @@ OUT:
 	if (unlikely(stat(dir, &g_st)))                      \
 		continue;                                    \
 	if (S_ISREG(g_st.st_mode)) {                         \
-		DO_REG(FUNC_REG, USE_LEN);                   \
+		FIND_FGREP_DO_REG(FUNC_REG, USE_LEN);        \
 	} else if (S_ISDIR(g_st.st_mode)) {                  \
-		DO_DIR(FUNC_SELF);                           \
+		FIND_FGREP_DO_DIR(FUNC_SELF);                \
 	}
 
 #endif /* _DIRENT_HAVE_D_TYPE */
@@ -231,25 +231,134 @@ static int F(const char *ptn, const size_t ptnlen, const char *dir, const size_t
 
 DEF_FIND_T(find_fgrep, fgrep, 1)
 
-/* static void usage(void) */
-/* { */
-/* 	puts("Usage: ./rgrep <ptn> <dir or file> \nif <dir or file> is not provided, it defaults to $PWD\n"); */
-/* 	exit(1); */
-/* } */
+static INLINE void catv(const char *RESTRICT filename, const size_t flen)
+{
+	FILE *RESTRICT fp = fopen(filename, "r");
+	if (unlikely(!fp))
+		return;
+	filename = filename + g_fuldirlen + 1;
+	g_lnp = g_ln;
+	CPY_N_ADV(g_lnp, ANSI_RED);
+	CPY_N_ADV_LEN(g_lnp, filename, flen);
+	CPY_N_ADV(g_lnp, ANSI_RESET ":" ANSI_GREEN "1" ANSI_RESET ":");
+	g_NL = 2;
+
+#define LOOP_CAT                                                  \
+	switch (*g_lnp = getc(fp)) {                              \
+	default:                                                  \
+	case '\t':                                                \
+		break;                                            \
+	case '\n':                                                \
+		fwrite(g_ln, 1, g_lnp - g_ln + 1, stdout);        \
+		g_lnp = g_ln;                                     \
+		CPY_N_ADV(g_lnp, ANSI_RED);                       \
+		CPY_N_ADV_LEN(g_lnp, filename, flen);             \
+		CPY_N_ADV(g_lnp, ANSI_RESET ":" ANSI_GREEN);      \
+		g_NLbufp = g_NLbuf;                               \
+		itoa_uint_pos(g_NLbufp, g_NL, 10, g_NLbufdigits); \
+		CPY_N_ADV_LEN(g_lnp, g_NLbufp, g_NLbufdigits);    \
+		CPY_N_ADV(g_lnp, ANSI_RESET ":");                 \
+		--g_lnp;                                          \
+		++g_NL;                                           \
+		break;                                            \
+	case EOF:                                                 \
+		*g_lnp = '\n';                                    \
+		fwrite(g_ln, 1, g_lnp - g_ln + 1, stdout);        \
+	case '\0':                                                \
+		CASE_UNPRINTABLE_WO_NUL_TAB_NL                    \
+		goto OUT;                                         \
+	}                                                         \
+	++g_lnp
+
+	do {
+		LOOP_CAT;
+		LOOP_CAT;
+		LOOP_CAT;
+		LOOP_CAT;
+	} while (MAX_LINE_LEN - 4 > (g_lnp - g_ln));
+OUT:
+	fclose(fp);
+}
+
+static void find_cat(const char *RESTRICT dir, const size_t dlen)
+{
+	DIR *RESTRICT dp = opendir(dir);
+	if (unlikely(!dp))
+		return;
+	struct dirent *RESTRICT ep;
+	char fulpath[MAX_PATH_LEN];
+	while ((ep = readdir(dp))) {
+#if DEBUG
+		printf("d->name: %s\n", ep->d_name);
+#endif /* DEBUG */
+
+#define FIND_CAT_DO_REG                                                                          \
+	catv(fulpath, appendp(fulpath, dir, dlen, ep->d_name) - (fulpath + g_fuldirlen) - 1)
+
+#define FIND_CAT_DO_DIR                                                               \
+	IF_EXCLUDED_DO(ep->d_name, continue)                                          \
+	if (pid > 0) {                                                                \
+		if (g_child_tot == g_child_max) {                                     \
+			wait(NULL);                                                   \
+			--g_child_tot;                                                \
+		}                                                                     \
+		pid = fork();                                                         \
+	}                                                                             \
+	switch (pid) {                                                                \
+	case 0:                                                                       \
+		find_cat(fulpath, appendp(fulpath, dir, dlen, ep->d_name) - fulpath); \
+		break;                                                                \
+	default:                                                                      \
+		++g_child_tot;                                                        \
+	case -1:;                                                                     \
+	}
+
+#ifdef _DIRENT_HAVE_D_TYPE
+		switch (ep->d_type) {
+		case DT_REG:
+			FIND_CAT_DO_REG;
+			break;
+		case DT_DIR:
+			FIND_CAT_DO_DIR;
+			break;
+		}
+#else
+		if (unlikely(stat(dir, &g_st)))
+			return;
+		if (S_ISREG(g_st.st_mode)) {
+			FIND_CAT_DO_REG;
+		} else if (S_ISDIR(g_st.st_mode)) {
+			FIND_CAT_DO_DIR;
+		}
+#endif /* _DIRENT_HAVE_D_TYPE */
+#if DEBUG
+		printf("entries: %s\n", ep->d_name);
+#endif /* DEBUG */
+	}
+	closedir(dp);
+}
+
+static void get_dir(char *buf)
+{
+	getcwd(buf, MAX_PATH_LEN);
+	g_fuldirlen = strlen(buf);
+}
 
 #define PTN_ argv[1]
 #define DIR_ argv[2]
 
 int main(int argc, char **argv)
 {
-	if (argc == 1 || !argv[0]) {
-		execlp("rcat", "rcat", NULL);
-		return EXIT_SUCCESS;
-	}
 	g_child_max = sysconf(_SC_NPROCESSORS_CONF);
 	if (unlikely(g_child_max == -1)) {
 		fputs("Can't get number of cores!", stderr);
 		return EXIT_FAILURE;
+	}
+	if (argc == 1 || !argv[1][0]) {
+		char cwd[MAX_PATH_LEN];
+		get_dir(cwd);
+		find_cat(cwd, g_fuldirlen);
+		return EXIT_SUCCESS;
 	}
 	char ptn[MAX_ARG_LEN];
 	char *ptnp = ptn;
@@ -290,8 +399,7 @@ int main(int argc, char **argv)
 	case '\0':
 	GET_CWD:;
 		char cwd[MAX_PATH_LEN];
-		getcwd(cwd, MAX_PATH_LEN);
-		g_fuldirlen = strlen(cwd);
+		get_dir(cwd);
 		find_fgrep(ptn, strlen(ptn), cwd, g_fuldirlen);
 		break;
 	}
